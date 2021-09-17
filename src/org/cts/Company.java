a972692e87280ce5201412c63753289dda12c862
Rajadurai07 <rajadurai07121994@gmail.com>
2021-09-17 15:28:22 +0530

    added company class

---
+++ src/org/cts/Company.java
@@ -0,0 +1,16 @@
+package org.cts;
+
+public class Company {
+
+	private void method1() {
+		System.out.println("method 1");
+
+	}
+	private void method2() {
+		System.out.println("method 2");
+
+	}
+	private void method3() {
+		System.out.println("mrthod 3");
+	}
+}
